daf94fa310052ef2978a7e8fa1d7456bf4278c5c
Mohammad Jafari <mamad.jafari91@gmail.com>
2023-07-22 09:35:15 +0330

    add unittest for set_width and set_height

--- rectangle.py
+++ rectangle.py
@@ -3,5 +3,17 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def set_width(self, width):
+        pass
+
+    def set_height(self, height):
+        pass
+
+    def get_width(self):
+        pass
+
+    def get_height(self):
+        pass
+
     def compute_area(self):
         return self.width * self.height
--- unit_test.py
+++ unit_test.py
@@ -6,3 +6,13 @@ class TestRectangle(unittest.TestCase):
     def test_compute_area(self):
         rectangle = Rectangle(5, 7)
         self.assertEqual(rectangle.compute_area(), 35)
+
+    def test_set_width(self):
+        rectangle = Rectangle(5, 7)
+        rectangle.set_width(10)
+        self.assertEqual(rectangle.get_width(), 10)
+
+    def test_set_height(self):
+        rectangle = Rectangle(5, 7)
+        rectangle.set_height(8)
+        self.assertEqual(rectangle.get_height(), 8)
